3b944dcc6b267a2ab39b351c1b2bf982a22abb50
amittell <mittell@me.com>
2018-02-21 13:57:23 -0500

    Added utility to split Windows wallet key
    split long key for simple wallet import

---
+++ split_xao_key.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl -l
+my $tempStr = $ARGV[0];
+die ("You didn't provide a key to split. Exiting.") if (!$tempStr);
+my @keys = split / /,(join ' ', unpack '(A64)*', $tempStr);
+print ("Public Spend Key: ",$keys[0]);
+print ("Public View keys: ",$keys[1]);
+print ("Private Spend Key: ",$keys[2]);
+print ("Private View Key: ",$keys[3]);
