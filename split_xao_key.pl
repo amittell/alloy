dccaf3166c8b3d3f8990692c6167326392676476
amittell <mittell@me.com>
2018-02-21 14:05:18 -0500

    Added --help option to key splitter util

--- split_xao_key.pl
+++ split_xao_key.pl
@@ -1,9 +1,12 @@
 #!/usr/bin/perl -l
 # Usage: split_xao_key.pl <LONG_KEY>
-my $tempStr = $ARGV[0];
-die ("You didn't provide a key to split. Exiting.") if (!$tempStr);
+print ("You didn't provide a key to split. Use --help for command info.") && exit 1 if (!$tempStr);
+if ($tempStr eq '--help') {
+        print ("Usage: split_xao_key.pl <LONG_KEY>");
+        exit 0;
+}
 my @keys = split / /,(join ' ', unpack '(A64)*', $tempStr);
-print ("Public Spend Key: ",$keys[0]);
+print ("\nPublic Spend Key: ",$keys[0]);
 print ("Public View keys: ",$keys[1]);
 print ("Private Spend Key: ",$keys[2]);
 print ("Private View Key: ",$keys[3]);
